d573a3cac48538522501d1d0e5d8ceb36716934f
Rick Way <thatrickguy@hotmail.com>
2014-06-19 23:07:09 -0500

    Fixes issue where auto loaded player data didn't update in the event players grid

--- DataStructure/Player.vb
+++ DataStructure/Player.vb
@@ -1,12 +1,57 @@
 ﻿Imports System.IO
 Imports System.Xml.Serialization
+Imports System.ComponentModel
 
 Public Class doPlayer
+    Implements INotifyPropertyChanged
+
+
     Public Property PlayerID As Guid
+
+    Private _Name As String
     Public Property Name As String
+        Get
+            Return _Name
+        End Get
+        Set(value As String)
+            _Name = value
+            OnPropertyChanged("Name")
+        End Set
+    End Property
+
+    Private _PPHandle As String
     Public Property PPHandle As String
+        Get
+            Return _PPHandle
+        End Get
+        Set(value As String)
+            _PPHandle = value
+            OnPropertyChanged("PPHandle")
+        End Set
+    End Property
+
+    Private _Meta As doMeta
     Public Property Meta As doMeta
+        Get
+            Return _Meta
+        End Get
+        Set(value As doMeta)
+            _Meta = value
+            OnPropertyChanged("Meta")
+        End Set
+    End Property
+
+    Private _Faction As doFaction
     Public Property Faction As doFaction
+        Get
+            Return _Faction
+        End Get
+        Set(value As doFaction)
+            _Faction = value
+            OnPropertyChanged("Faction")
+        End Set
+    End Property
+
     Public Property ByeVol As Boolean
     Public Property Drop As Boolean
     Public Property TourneyPoints As Integer
@@ -28,6 +73,13 @@ Public Class doPlayer
         dopReturn.PPHandle = Me.PPHandle
         Return dopReturn
     End Function
+
+
+    ' Create the OnPropertyChanged method to raise the event 
+    Protected Sub OnPropertyChanged(ByVal name As String)
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(name))
+    End Sub
+    Public Event PropertyChanged(sender As Object, e As PropertyChangedEventArgs) Implements INotifyPropertyChanged.PropertyChanged
 End Class
 
 Public Class doPlayerCollection
