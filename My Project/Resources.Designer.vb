e265c3152d74a2d9a6bca66e208c63592f4d48f7
Rick Way <thatrickguy@hotmail.com>
2014-06-21 14:32:09 -0500

    Added pairing list printing

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -59,5 +59,81 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to &lt;div id=&quot;vr&quot;&gt;
+        '''		&lt;/div&gt;
+        '''	&lt;/div&gt;
+        '''&lt;/body&gt;.
+        '''</summary>
+        Friend ReadOnly Property Footer() As String
+            Get
+                Return ResourceManager.GetString("Footer", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to &lt;div id=&quot;header&quot;&gt;
+        '''	&lt;h1&gt;{0}&lt;/h1&gt;
+        '''	&lt;h2 style=&quot;margin-top:-20px;&quot;&gt;Round {1} Pairings&lt;/h2&gt;
+        '''&lt;/div&gt;.
+        '''</summary>
+        Friend ReadOnly Property Header() As String
+            Get
+                Return ResourceManager.GetString("Header", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to &lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.01//EN&quot; &quot;http://www.w3.org/TR/html4/strict.dtd&quot;&gt;
+        '''&lt;html lang=&quot;en&quot;&gt;
+        '''&lt;head&gt;
+        '''	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=utf-8&quot;&gt;
+        '''	&lt;title&gt;Simple 2 column CSS layout, step 3 | 456 Berea Street&lt;/title&gt;
+        '''	&lt;meta name=&quot;description&quot; content=&quot;How to create a simple two column CSS layout with full width header and footer.&quot;&gt;
+        '''	&lt;meta name=&quot;copyright&quot; content=&quot;Copyright (c) 2004 Roger Johansson&quot;&gt;
+        '''	&lt;meta name=&quot;author&quot; content=&quot;Roger Johansson&quot;&gt;
+        '''	&lt;style type=&quot;t [rest of string was truncated]&quot;;.
+        '''</summary>
+        Friend ReadOnly Property HTMLHeader() As String
+            Get
+                Return ResourceManager.GetString("HTMLHeader", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to &lt;div id=&quot;Item&quot;&gt;
+        '''	&lt;span id=&quot;Player&quot;&gt;{0}&lt;/span&gt;&lt;br&gt;
+        '''	&lt;span id=&quot;Table&quot;&gt;Table {1}&lt;/span&gt;
+        '''	&lt;span id=&quot;Opponent&quot;&gt;vs {2}&lt;/span&gt;
+        '''&lt;/div&gt;.
+        '''</summary>
+        Friend ReadOnly Property Item() As String
+            Get
+                Return ResourceManager.GetString("Item", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to &lt;div id=&quot;LeftColumn&quot;&gt;
+        '''	{0}
+        '''&lt;/div&gt;.
+        '''</summary>
+        Friend ReadOnly Property LeftColumn() As String
+            Get
+                Return ResourceManager.GetString("LeftColumn", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to &lt;div id=&quot;RightColumn&quot;&gt;
+        '''	{0}
+        '''&lt;/div&gt;.
+        '''</summary>
+        Friend ReadOnly Property RightColumn() As String
+            Get
+                Return ResourceManager.GetString("RightColumn", resourceCulture)
+            End Get
+        End Property
     End Module
 End Namespace
--- UserControls/Pairings.xaml.vb
+++ UserControls/Pairings.xaml.vb
@@ -1,4 +1,6 @@
-﻿Public Class Pairings
+﻿Imports System.Text
+
+Public Class Pairings
 
     Private Sub btnGeneratePairing_Click(sender As Object, e As RoutedEventArgs) Handles btnGeneratePairing.Click
         btnGeneratePairing.Content = "Regenerate Pairings"
@@ -8,16 +10,62 @@
     End Sub
 
     Private Sub btnPrintPairing_Click(sender As Object, e As RoutedEventArgs) Handles btnPrintPairing.Click
+        Dim sbOutput As New StringBuilder
+        sbOutput.Append(My.Resources.HTMLHeader)
+        sbOutput.AppendFormat(My.Resources.Header, BaseController.Model.WMEvent.Name, BaseController.Model.CurrentRound.RoundNumber)
 
+        Dim sbLeftBlock As New StringBuilder()
+        Dim sbRightBlock As New StringBuilder()
+
+        Dim PlayerGames As New List(Of PlayerGame)
+        For Each player In (From p In BaseController.Model.CurrentRound.Players Order By p.Name)
+            Dim game = (From p In BaseController.Model.CurrentRound.Games Where p.Player1.PPHandle = player.PPHandle Or (p.Player2 IsNot Nothing AndAlso p.Player2.PPHandle = player.PPHandle)).FirstOrDefault
+
+            Dim pg As PlayerGame = Nothing
+            If game.Player2 Is Nothing Then
+                'bye
+                pg.Player = game.Player1.Name
+                pg.Table = "-"
+                pg.Opponent = "Bye"
+            ElseIf game.Player1.PPHandle = player.PPHandle Then
+                pg.Player = game.Player1.Name
+                pg.Table = game.TableNumber
+                pg.Opponent = game.Player2.Name
+            ElseIf game.Player2.PPHandle = player.PPHandle Then
+                pg.Player = game.Player2.Name
+                pg.Table = game.TableNumber
+                pg.Opponent = game.Player1.Name
+            End If
+            PlayerGames.Add(pg)
+        Next
+
+        For i As Integer = 0 To Math.Ceiling(PlayerGames.Count / 2)
+            sbLeftBlock.AppendFormat(My.Resources.Item, New String() {PlayerGames(i).Player, PlayerGames(i).Table, PlayerGames(i).Opponent})
+            sbRightBlock.AppendFormat(My.Resources.Item, New String() {PlayerGames(i + Math.Floor(PlayerGames.Count / 2)).Player, PlayerGames(i + Math.Floor(PlayerGames.Count / 2)).Table, PlayerGames(i + Math.Floor(PlayerGames.Count / 2)).Opponent})
+        Next
+
+        sbOutput.AppendFormat(My.Resources.LeftColumn, sbLeftBlock.ToString)
+        sbOutput.AppendFormat(My.Resources.RightColumn, sbRightBlock.ToString)
+        sbOutput.Append(My.Resources.Footer)
+
+
+        IO.File.WriteAllText(".\PairingsList.html", sbOutput.ToString)
+        Process.Start(".\PairingsList.html")
     End Sub
 
     Private Sub btnSwap_Click(sender As Object, e As RoutedEventArgs) Handles btnSwap.Click
         If cboSwapPlayerq1.SelectedItem IsNot Nothing AndAlso cboSwapPlayerq2.SelectedItem IsNot Nothing Then
             CType(BaseController.CurrentController, PairingsController).SwapPlayers(cboSwapPlayerq1.SelectedValue, cboSwapPlayerq2.SelectedValue)
-           
+
             Me.cboSwapPlayerq1.SelectedItem = Nothing
             Me.cboSwapPlayerq2.SelectedItem = Nothing
             Me.dgPairings.Items.Refresh()
         End If
     End Sub
 End Class
+
+Public Structure PlayerGame
+    Public Player As String
+    Public Table As Integer
+    Public Opponent As String
+End Structure
