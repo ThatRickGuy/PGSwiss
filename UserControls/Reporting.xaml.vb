e73cd591e24b2e7a93a78e2251c15aa43c684bc1
ThatRickGuy <thatrickguy@hotmail.com>
2014-06-23 17:51:57 -0500

    open the hyperlink HAL!

--- UserControls/Reporting.xaml.vb
+++ UserControls/Reporting.xaml.vb
@@ -1,2 +1,7 @@
 ﻿Public Class Reporting
+
+    Private Sub Hyperlink_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+        Process.Start(New ProcessStartInfo(e.Uri.AbsoluteUri))
+        e.Handled = True
+    End Sub
 End Class
