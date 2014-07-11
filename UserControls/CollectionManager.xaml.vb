6fa4e30cc81ca4e7b531d12ed5c25c9dcfefff40
ThatRickGuy <thatrickguy@hotmail.com>
2014-07-11 11:25:06 -0500

    Event Format support in collection manager

--- UserControls/CollectionManager.xaml.vb
+++ UserControls/CollectionManager.xaml.vb
@@ -1,11 +1,17 @@
-﻿Public Class CollectionManager
+﻿Imports PGSwiss.Data
+
+Public Class CollectionManager
 
     Private Sub cboCollection_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cboCollection.SelectionChanged
         Dim Controller = CType(BaseController.CurrentController, CollectionManagerController)
         Dim Holder As New List(Of StringHolder)
         Select Case CType(cboCollection.SelectedItem, ComboBoxItem).Content
             Case Is = "Event Formats"
-                Me.dgCollection.ItemsSource = Controller.EventFormats
+                Dim h As New List(Of StringAndStringCollectionHolder)
+                For Each EventFormat In Controller.EventFormats
+                    h.Add(New StringAndStringCollectionHolder(EventFormat))
+                Next
+                Me.dgCollection.ItemsSource = h
             Case Is = "Game Sizes"
                 Dim q = From p In Controller.Sizes Select New StringHolder With {.Value = p.ToString}
                 Holder.AddRange(q)
@@ -40,14 +46,47 @@
         Public Property Value As String
     End Class
 
+    Public Class StringAndStringCollectionHolder
+        Public Sub New(EventFormat As doEventFormat)
+            Value = EventFormat.Name
+            _StringCollection = EventFormat.Scenarios
+        End Sub
+
+        Public Property Value As String
+        Private _StringCollection As New List(Of String)
+        Public Property StringCollection As String
+            Get
+                Dim sReturn As String = String.Empty
+                For Each s In _StringCollection
+                    sReturn &= s & ", "
+                Next
+                If sReturn.EndsWith(", ") Then sReturn = sReturn.Substring(0, sReturn.Length - 2)
+                Return sReturn
+            End Get
+            Set(value As String)
+                Dim values = value.Split(",")
+                _StringCollection.Clear()
+                For Each s In values
+                    _StringCollection.Add(s.TrimEnd)
+                Next
+            End Set
+        End Property
+
+        Public Function GetActualCollection() As List(Of String)
+            Return _StringCollection
+        End Function
+    End Class
+
     Private Sub dgCollection_LostFocus(sender As Object, e As RoutedEventArgs) Handles dgCollection.LostFocus
         Dim Holder As List(Of StringHolder)
         Dim Controller = CType(BaseController.CurrentController, CollectionManagerController)
         Select Case CType(cboCollection.SelectedItem, ComboBoxItem).Content
             Case Is = "Event Formats"
-                Holder = Me.dgCollection.ItemsSource
+                Dim h As List(Of StringAndStringCollectionHolder) = Me.dgCollection.ItemsSource
                 Controller.EventFormats.Clear()
-                Controller.EventFormats.AddRange(From p In Holder Select p.Value)
+                For Each i In h
+                    Controller.EventFormats.Add(New doEventFormat With {.Name = i.Value, .Scenarios = i.GetActualCollection})
+                Next
             Case Is = "Game Sizes"
                 Holder = Me.dgCollection.ItemsSource
                 Controller.Sizes.Clear()
